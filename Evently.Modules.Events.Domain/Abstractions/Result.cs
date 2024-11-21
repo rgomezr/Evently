c6ddccff44fa8f58f7b5aada691b77682867b441
Ramón Gómez <ramon@ramongomez.xyz>
2024-11-21 08:55:23 +0100

    Within Domain: - Created ErrorType, Error abstractions - Started creating Result and Result<TValue> abstractions

---
+++ Evently.Modules.Events.Domain/Abstractions/Result.cs
@@ -0,0 +1,33 @@
+namespace Evently.Modules.Events.Domain.Abstractions;
+
+public class Result
+{
+    public Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException($"Invalid Error: {nameof(error)}");
+        }
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    
+    public static Result Success() => new(true, Error.None);
+
+    // public static Result<TValue> Success<TValue>(TValue value) =>
+    //     new(value, true, Error.None);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    public Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
+    {
+        _value = value;
+    }
+}
---
+++ Evently.Modules.Events.Domain/Categories/CategoryErrors.cs
@@ -0,0 +1,7 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Evently.Modules.Events.Domain.Events;
+
+public static class CategoryErrors
+{
+}
