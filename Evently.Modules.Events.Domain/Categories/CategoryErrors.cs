3dedb9de54efebab21a4ffd18b176a25abe15533
Ramón Gómez <ramon@ramongomez.xyz>
2024-11-22 09:46:12 +0100

    Domain.Abstractions - Completed CategoryErrors class

--- Evently.Modules.Events.Domain/Categories/CategoryErrors.cs
+++ Evently.Modules.Events.Domain/Categories/CategoryErrors.cs
@@ -1,7 +1,13 @@
 using System.Runtime.InteropServices.JavaScript;
+using Evently.Modules.Events.Domain.Abstractions;
 
 namespace Evently.Modules.Events.Domain.Events;
 
 public static class CategoryErrors
 {
+    public static Error NotFound(Guid categoryId) =>
+        Error.NotFound("Categories.NotFound",
+            $"The category with id: {categoryId} does not exist.");
+    public static readonly Error AlreadyArchived = Error.Problem("Categories.AlreadyArchived",
+        "The category is already archived.");
 }
