3d5989341601f90866493cf32b3e9a337cca3aac
Ramón Gómez <ramon@ramongomez.xyz>
2025-06-13 08:27:12 +0200

    Added TicketTypeErrors and ITicketTypeRepository

---
+++ Evently.Modules.Events.Domain/TicketTypes/ITicketTypeRepository.cs
@@ -0,0 +1,8 @@
+namespace Evently.Modules.Events.Domain.TicketTypes;
+
+public interface ITicketTypeRepository
+{
+    Task<TicketType?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    void Insert(TicketType ticketType);
+}
